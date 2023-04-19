d566d35a09f0466e853e010f2857822ff654b40d
zp <zeropara@gmail.com>
2023-04-19 14:29:24 +0200

    removed log

--- web/src/lib/auth/authStore.js
+++ web/src/lib/auth/authStore.js
@@ -2,8 +2,4 @@ import { writable } from 'svelte/store';
 import { variables } from '$lib/envVariables';
 import { Buffer } from 'buffer';
 
-
-console.log(variables)
-console.log(import.meta.env)
-
 export const authToken = writable(`Basic ${Buffer.from(variables.apiKey).toString('base64')}`);
