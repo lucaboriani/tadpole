a5ae8137714f6ee4e384ded645df52dd7a5c3c82
zp <zeropara@gmail.com>
2023-05-09 14:50:45 +0200

    added adapter

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,11 +1,12 @@
 //import adapter from '@sveltejs/adapter-auto';
 // import adapter from '@sveltejs/adapter-node';
-import adapter from '@sveltejs/adapter-static';
+//import adapter from '@sveltejs/adapter-static';
+import adapter from '@eslym/sveltekit-adapter-openwhisk';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
+		/* adapter: adapter({
             // default options are shown. On some platforms
             // these options are set automatically — see below
             pages: 'build',
@@ -13,6 +14,12 @@ const config = {
             fallback: null,
             precompress: false,
             strict: true
+        }) */
+        adapter: adapter({
+            // the base url for the function, you will need to specify kit.paths.base when its not on root
+            baseUrl: process.env.VITE_PUBILC_OW_API_HOST,
+            // header to get connecting ip, 'do-connecting-ip' is an example for DigitalOcean App Platform
+            ipHeader: 'do-connecting-ip'
         })
 	},
 	preprocess: vitePreprocess()
